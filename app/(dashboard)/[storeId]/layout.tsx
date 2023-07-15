712d851a1b1505abfd86eb2c5a2853613ad47877
cieko <rahulsaxena2021@hotmail.com>
2023-07-15 12:54:14 +0530

    navbar part 1

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 
-// import Navbar from '@/components/navbar'
+import Navbar from '@/components/navbar'
 import prismadb from "@/lib/prismadb";
 
 export default async function DashboardLayout({
@@ -30,7 +30,7 @@ export default async function DashboardLayout({
 
     return (
         <>
-            {/* <Navbar /> */}
+            <Navbar />
             {children}
         </>
     );
